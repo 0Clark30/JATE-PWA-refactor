911fe953bc70d66f84f25f9b6bc40e217e4ecf13
0Clark30 <austinlaw.clark@outlook.com>
2024-03-06 13:13:02 -0600

    added asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+offlineFallback({
+  pageFallback: "/offline.html"
+});
+registerRoute(
+  ({request}) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      })
+    ]
+  })
+);
