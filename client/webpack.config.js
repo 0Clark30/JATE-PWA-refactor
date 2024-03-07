1d94c83b63c59b32db24b024443f571ba0973dc5
0Clark30 <austinlaw.clark@outlook.com>
2024-03-06 20:10:29 -0600

    deployment

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,20 +31,13 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
-        description: "Never forget your contacts!",
+        name: "JATE",
+        short_name: "JATE",
+        description: "Just another Text Editor",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "./",
         publicPath: "./",
-        icons: [
-          {
-            src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
-          },
-        ],
       }),
     ],
 
@@ -53,6 +46,21 @@ module.exports = () => {
         {
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+       
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
         },
       ],
     },
